be40176849a4509f72f18caf053a1b6764c141c4
Rahuldeb5 <rahuldeb.usa@gmail.com>
2025-10-02 13:04:28 -0400

    Switched to importing images to display during deployment

--- src/comps/Footer.jsx
+++ src/comps/Footer.jsx
@@ -1,6 +1,11 @@
 import { Box, Link, Typography } from "@mui/material";
 import "./Footer.css";
 
+import LinktreeImage from "../../images/linktree-icon-transparent.png";
+import InstagramImage from "../../images/instagram-logo-transparent.png";
+import FacebookImage from "../../images/facebook-logo-transparent.png";
+import StuyImage from "../../images/stuyvesant-high-school-logo-transparent.png";
+
 const Footer = () => {
     return (
         <Box className="main-footer">
@@ -14,7 +19,7 @@ const Footer = () => {
                         <Link href="https://linktr.ee/seniorcaucus2026" target="_blank" rel="noopener noreferrer" className="link-item">
                             <Box
                                 component="img" 
-                                src="../../images/linktree-icon-transparent.png" 
+                                src={LinktreeImage}
                                 alt="link-tree" 
                                 className="icon-image"
                             />
@@ -22,7 +27,7 @@ const Footer = () => {
                         <Link href="https://www.instagram.com/stuyseniorcaucus/" target="_blank" rel="noopener noreferrer" className="link-item">
                             <Box
                                 component="img" 
-                                src="../../images/instagram-logo-transparent.png" 
+                                src={InstagramImage}
                                 alt="insta" 
                                 className="icon-image"
                             />
@@ -30,7 +35,7 @@ const Footer = () => {
                         <Link href="https://www.facebook.com/share/g/1D2kZEi262/" target="_blank" rel="noopener noreferrer" className="link-item">
                             <Box
                                 component="img" 
-                                src="../../images/facebook-logo-transparent.png" 
+                                src={FacebookImage}
                                 alt="FB" 
                                 className="icon-image"
                             />
@@ -42,7 +47,7 @@ const Footer = () => {
                     <Link href="https://stuy.enschool.org/" target="_blank" rel="noopener noreferrer" className="link-item">
                         <Box
                             component="img" 
-                            src="../../images/stuyvesant-high-school-logo-transparent.png" 
+                            src={StuyImage}
                             alt="stuyvesant-high-school" 
                             className="brand-logo"
                         />
